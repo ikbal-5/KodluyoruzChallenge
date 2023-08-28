1f5dc646380e0b7b558fad6a267ee56f54e58ab0
ikbal <73275697+ikbal-5@users.noreply.github.com>
2023-08-28 17:39:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+#🙌🏼 Easy: Bir futbol maçında 3 puanlık atışlarda 5 kez isabet edildi, 2 puanlık atışlarda ise 10 kez isabet edildi. Toplam kaç puan elde edildi?
+uatıs = 5
+ıatıs = 10
+upuan = 3
+ıpuan = 2
+toplamPuan = (uatıs*upuan) + (ıatıs*ıpuan)
+print(toplamPuan)
+#🌟Medium:  Bir mağazada bir kitap 80 TL, bir defter 20 TL ve bir kalem 5 TL. Bir müşteri 2 kitap, 3 defter ve 4 kalem aldı. Müşteri ne kadar ödeme yapmalı?
+kitap = 80
+defter = 20
+kalem = 5
+def hesap():
+    print("Toplam Tutar : ",2*kitap+3*defter+4*kalem)
+hesap()
